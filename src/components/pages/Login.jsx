9d352589393c5e227fecd4a6505684efa33d945e
kalosius <kalosius43@gmail.com>
2023-10-15 08:32:39 +0300

    login interface modified

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,10 +1,10 @@
 import React, {useState} from "react"
 import { Link } from "react-router-dom";
 
+
 export const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pswd, setPswd] = useState('');
-
 
     const handleSubmit =(e) =>{
         e.preventDefault();
@@ -12,44 +12,53 @@ export const Login = (props) => {
 
     }
     return (
-      <div className="auth-form-container login">
-        <h2> Hello Again </h2>
-        <h1> Login with </h1>
-        <button><img className="login" src="imgs/login/google_logo.png" alt="" /></button>
-        <button><img className="login" src="imgs/login/Facebook-Logo-2019.png" alt="facebookLogo" /></button>
-        <form className="login-form" onSubmit={handleSubmit}>
-          <label htmlfor="email"></label>
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-            placeholder="Username/Email"
-            id="email"
-            name="email"
-          />
-          <label htmlfor="password">  </label>
-          <input
-            value={pswd}
-            onChange={(e) => setPswd(e.target.value)}
-            type="password"
-            placeholder="Password"
-            id="password"
-            name="password"
-          />
-          <button type="submit"> Log In </button>
-          <div>
-            <a className="log" href="#">Remember me</a><a href="#">forgot password?</a>
+      <div className="most-out-block">
+        <div className="main-container">
+          <div  className="login-side-list">
+            <div className="line-breaks">
+              <h1><br/> <br/>Indulge <br/>in <br/> Ugandan <br/> Diversity</h1>
+            </div>
           </div>
-        </form>
-        <Link to={"/Register"}>
-          <button
-            // className="link-btn"
-            // onClick={() => props.onFormSwitch('Login')}
-          className="underlog-links">
-            Register/SignUp
-          </button>
-          <img className="login-logo llogin" src="imgs/login/fgfoundation_logo.png" alt="" />
-        </Link>
+          <div className="auth-form-container login ">
+            <h2> Hello Again </h2>
+            <h1> Login with </h1>
+            <button><img className="login-logo" src="imgs/login/google_logo.png" alt="" /></button>
+            <button><img className="login-logo" src="imgs/login/Facebook-Logo-2019.png" alt="facebookLogo" /></button>
+            <form className="login-form" onSubmit={handleSubmit}>
+              <label htmlfor="email"></label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Username/Email"
+                id="email"
+                name="email"
+              />
+              <label htmlfor="password">  </label>
+              <input
+                value={pswd}
+                onChange={(e) => setPswd(e.target.value)}
+                type="password"
+                placeholder="Password"
+                id="password"
+                name="password"
+              />
+              <button type="submit"> Log In </button>
+              <div>
+                <a className="log" href="#">Remember me</a><a href="#">forgot password?</a>
+              </div>
+            </form>
+            <Link to={"/Register"}>
+              <button
+                // className="link-btn"
+                // onClick={() => props.onFormSwitch('Login')}
+              className="underlog-links">
+                Register/SignUp
+              </button>
+              <img className="login-logo llogin" src="imgs/login/fgfoundation_logo.png" alt="" />
+            </Link>
+          </div>
+        </div>
       </div>
       
     );
