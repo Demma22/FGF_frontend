b78eb769819ba2f88d39c2eef272f4f1307ae7e0
OLIVE <bon_olives@yahoo.com>
2023-12-13 12:09:22 +0300

    Added Contributor Dashboard

---
+++ src/components/ContributorDashboard/data/ContributorData.js
@@ -0,0 +1,15 @@
+export const contributorData = [
+    {
+        id: 1,
+        name: {
+            first: 'Paula',
+            last: 'Koru',
+        },
+        email: "paula@koru.com",
+        age: 19,
+        phone: "(256)123-4567",
+        access: "admincontributor",
+        isAdmin: true,
+    },
+
+];
